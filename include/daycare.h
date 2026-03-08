177cbfaa5151f2db1bfa6c510009e4dd39948a4b
Exclsior <D@DMendis.com>
2026-03-08 15:45:07 +0000

    Updated Egg Moves to be shown on all evolution stages and at the end of the list.

--- include/daycare.h
+++ include/daycare.h
@@ -34,5 +34,6 @@ u8 GetEggMoves(struct Pokemon *pokemon, u16 *eggMoves);
 
 u8 GetEggMovesSpecies(u16 species, u16 *eggMoves);
 bool8 SpeciesCanLearnEggMove(u16 species, u16 move);
+u16 GetEggSpecies(u16 species);
 
 #endif // GUARD_DAYCARE_H
